73e4d268783b412431e691040d73ed965e92f008
Owen Bress <39778413+OBress@users.noreply.github.com>
2024-12-27 19:08:02 -0500

    Delete backend directory

--- backend/routes/makerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createVideos } from '../controllers/makerController.js';
-import { authenticateToken } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.post('/create-videos', authenticateToken, createVideos);
-
-export default router;
